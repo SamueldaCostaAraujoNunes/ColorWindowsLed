6536e9ee874cbb312d0ce3568791c060b5c22159
Samuel da Costa Araujo Nunes <samueldacostaaraujonunes@gmail.com>
2020-10-08 10:35:01 -0300

    Adcionando Herança ao Windows_Color.py

--- windows_color.py
+++ windows_color.py
@@ -1,10 +1,10 @@
 from winreg import HKEY_CURRENT_USER, ConnectRegistry, OpenKey, QueryValueEx
 from typing import Tuple
 from color import Color
-class WindowsColor():
+class WindowsColor(Color):
     def __init__(self):
         #self.color = self.hex_to_rgb(self.color_current()[3])
-        self.color = Color(self.color_current()[3], "HEX")
+        super().__init__(self.color_current()[3], "HEX")
 
     def color_current(self) -> list:
         registry = ConnectRegistry(None, HKEY_CURRENT_USER)
@@ -15,14 +15,14 @@ class WindowsColor():
         list_color = ['#' + colors[color:color+6] for color in range(0, 64, 8)]
         return list_color
 
-    def get_color(self, max=False, standard=None):
-        return self.color.get_color(max=max, standard=standard)
+    # def get_color(self, max=False, standard=None):
+    #     return self.color.get_color(max=max, standard=standard)
 
-    def set_color(self, color, standard):
-        self.color.set_color(color, standard)
+    # def set_color(self, color, standard):
+    #     self.color.set_color(color, standard)
 
 if __name__ == "__main__":
     cor = WindowsColor()
-    print(cor.get_color(max=True, standard='RGB'))
+    print(cor.get_color(max=False, standard='heX'))
     cor.set_color([240,124,23], "RGB")
     print(cor.get_color(max=True, standard='HEX'))
